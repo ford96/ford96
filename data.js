822f98514534f5a4d37406e1847a143e492695d4
Ainur <hak.ain@yandex.ru>
2018-01-09 03:25:47 +0500

    edit finish

---
+++ data.js
@@ -0,0 +1,197 @@
+var data = [
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://ezoera.com/wp-content/uploads/2015/09/1441818355-b18ace7cb299850e4c58590b8d711c88.png",
+        "x" : "60.601",
+        "y" : "56.804",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://a.d-cd.net/f846d3u-960.jpg",
+        "x" : "60.601",
+        "y" : "56.817",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://cs.pikabu.ru/images/big_size_comm/2012-08_4/13450971887351.jpg",
+        "x" : "60.609",
+        "y" : "56.823",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://vash-fenshyu.ru/wp-content/uploads/2014/06/nomer-mashiny-po-fen-shuj-vosem.jpg",
+        "x" : "60.609",
+        "y" : "56.839",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://www.listname.ru/gadanie/img/original-533x400.jpg",
+        "x" : "60.605",
+        "y" : "56.845",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://www.vzsar.ru/i/news/medium/2016/01/117630.jpg",
+        "x" : "60.614",
+        "y" : "56.854",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://ezoera.com/wp-content/uploads/2015/09/1441818355-b18ace7cb299850e4c58590b8d711c88.png",
+        "x" : "60.605",
+        "y" : "56.864",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://a.d-cd.net/f846d3u-960.jpg",
+        "x" : "60.624",
+        "y" : "56.872",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://cs.pikabu.ru/images/big_size_comm/2012-08_4/13450971887351.jpg",
+        "x" : "60.613",
+        "y" : "56.880",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://vash-fenshyu.ru/wp-content/uploads/2014/06/nomer-mashiny-po-fen-shuj-vosem.jpg",
+        "x" : "60.69",
+        "y" : "56.89",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://www.listname.ru/gadanie/img/original-533x400.jpg",
+        "x" : "60.684",
+        "y" : "56.790",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://www.vzsar.ru/i/news/medium/2016/01/117630.jpg",
+        "x" : "60.67",
+        "y" : "56.79",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://ezoera.com/wp-content/uploads/2015/09/1441818355-b18ace7cb299850e4c58590b8d711c88.png",
+        "x" : "60.669",
+        "y" : "56.881",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://a.d-cd.net/f846d3u-960.jpg",
+        "x" : "60.653",
+        "y" : "56.859",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://cs.pikabu.ru/images/big_size_comm/2012-08_4/13450971887351.jpg",
+        "x" : "60.645",
+        "y" : "56.817",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://vash-fenshyu.ru/wp-content/uploads/2014/06/nomer-mashiny-po-fen-shuj-vosem.jpg",
+        "x" : "60.636",
+        "y" : "56.817",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://www.listname.ru/gadanie/img/original-533x400.jpg",
+        "x" : "60.628",
+        "y" : "56.815",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://www.vzsar.ru/i/news/medium/2016/01/117630.jpg",
+        "x" : "60.617",
+        "y" : "56.833",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://ezoera.com/wp-content/uploads/2015/09/1441818355-b18ace7cb299850e4c58590b8d711c88.png",
+        "x" : "60.6012",
+        "y" : "56.8157",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://a.d-cd.net/f846d3u-960.jpg",
+        "x" : "60.5925",
+        "y" : "56.8293",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://cs.pikabu.ru/images/big_size_comm/2012-08_4/13450971887351.jpg",
+        "x" : "60.584",
+        "y" : "56.815",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "https://vash-fenshyu.ru/wp-content/uploads/2014/06/nomer-mashiny-po-fen-shuj-vosem.jpg",
+        "x" : "60.573",
+        "y" : "56.824",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://www.listname.ru/gadanie/img/original-533x400.jpg",
+        "x" : "60.562",
+        "y" : "56.823",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    },
+    {
+        "worker" : "Mark",
+        "imageurl" : "http://www.vzsar.ru/i/news/medium/2016/01/117630.jpg",
+        "x" : "60.551",
+        "y" : "56.812",
+        "date" : "15.12.2017",
+        "number" : "A111BB96"
+    }    
+];
+
+		
--- index.md
+++
@@ -1,3 +0,0 @@
-# FORD96 PAGE
-
-[OPEN MAP](op/markerpopups.html)
